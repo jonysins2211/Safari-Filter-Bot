ba720a61c03711ed412c7211ec70abc8764e15c8
jonysins2211 <Jonysins11220@gmail.com>
2025-06-30 22:32:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ LIMIT_MODE = is_enabled((environ.get('LIMIT_MODE', 'False')), False)
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '949657126').split()]
 OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "Safaridev") # widout 👉 @
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002096410592 -1001176901973').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002096410592').split()]
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '-1002549330105').split(','))))
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001622914589'))
